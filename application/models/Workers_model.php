5e68074e621313dff107c776b1dd267e07240408
Darek <dedent72@gmail.com>
2024-01-18 17:17:31 +0100

    Worker hierarchy fix

--- application/models/Workers_model.php
+++ application/models/Workers_model.php
@@ -7,48 +7,62 @@ class Workers_model extends CI_Model
 		$this->load->database();
 	}
 
-	public function get_workers() {
+	public function get_workers()
+	{
 		$query = $this->db->get('workers');
 		return $query->result_array();
 	}
 
-	public function get_worker($id) {
+	public function get_worker($id)
+	{
 		$query = $this->db->get_where('workers', array('id' => $id));
 		return $query->row_array();
 	}
 
-	public function add_worker($data) {
+	public function add_worker($data)
+	{
 		$data['position_id'] = $this->input->post('position');
 
 		return $this->db->insert('workers', $data);
 	}
 
-	public function update_worker($id, $data) {
+	public function update_worker($id, $data)
+	{
 		$data['position_id'] = $this->input->post('position');
 
 		$this->db->where('id', $id);
 		return $this->db->update('workers', $data);
 	}
 
-	public function delete_worker($id) {
+	public function delete_worker($id)
+	{
 		return $this->db->delete('workers', array('id' => $id));
 	}
 
-	public function update_worker_manager($worker_id, $manager_id) {
+	public function update_worker_manager($worker_id, $manager_id)
+	{
 		$data = array('manager_id' => $manager_id);
 		$this->db->where('id', $worker_id);
 		$this->db->update('workers', $data);
 	}
 
-	public function get_workers_hierarchy() {
-		$query = $this->db->query("
-            SELECT e.id, e.name, e.surname, e.position_id, e.manager_id,
-                   m.name AS manager_name, m.surname AS manager_surname
-            FROM workers e
-            LEFT JOIN workers m ON e.manager_id = m.id
-            ORDER BY e.manager_id ASC, e.id ASC
-        ");
+	public function get_workers_hierarchy($managerId = NULL)
+	{
+		$this->db->select('*');
+		$this->db->from('workers');
+		if ($managerId !== null) {
+			$this->db->where('manager_id', $managerId);
+		} else {
+			$this->db->or_where('manager_id', 0);
+		}
+		$query = $this->db->get();
 
-		return $query->result_array();
+		$result = $query->result_array();
+
+		foreach ($result as &$row) {
+			$row['subordinates'] = $this->get_workers_hierarchy($row['id']);
+		}
+
+		return $result;
 	}
 }
